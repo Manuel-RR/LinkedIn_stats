a61f844f24133e68dd8a3e95db55911f0a1f627e
manu19064 <manuelritschruiz@gmail.com>
2023-10-04 17:51:49 +0200

    corrected path gc error

--- src/analyzer.py
+++ src/analyzer.py
@@ -7,15 +7,15 @@ warnings.filterwarnings(action='ignore')
 
 try:
   file_folder = join(os.getcwd(),'profile_data')
+  file_name = os.listdir(file_folder)[0]
 
 except:
   print('Google colab enviroment detected!')
   file_folder = join(os.getcwd(),'LinkedIn_stats','profile_data')
+  file_name = os.listdir(file_folder)[0]
 
-file_name = os.listdir(file_folder)[0]
 FILE = pd.ExcelFile(join(file_folder,file_name))
 print(f'Succesfully loaded: {file_name}')
-
 
 def stats_by_month(data:pd.DataFrame) -> pd.DataFrame:
   return data.groupby(data.index.month_name()).describe()
